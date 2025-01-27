ad5cd783607b5e764c899156fd7efecb5fd70419
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2025-01-27 10:13:19 -0300

    commit

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,7 +3,10 @@ import { NextResponse } from 'next/server';
 //login
 export async function GET() {
     const { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } = process.env;
-    
+
+    console.log("--------------------- Environment ----------------------")
+    console.log(SPOTIFY_REDIRECT_URI)
+
     const state = Math.random().toString(36).substring(2, 15);
     const scope = 'user-read-private user-read-email user-top-read playlist-modify-public';
 
@@ -17,3 +20,15 @@ export async function GET() {
 
     return NextResponse.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
 }
+
+/* 
+
+https://accounts.spotify.com/authorize?response_type=code&client_id=ab80c067ae70469a91a83005ff894aae&scope=user-read-private+user-read-email+user-top-read+playlist-modify-public&
+// redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fcallback&state=wbcxb7imvw
+*/
+
+/* 
+
+https://accounts.spotify.com/authorize?response_type=code&client_id=ab80c067ae70469a91a83005ff894aae&scope=user-read-private+user-read-email+user-top-read+playlist-modify-public
+// &redirect_uri=undefined&state=xsydpzpy8r
+*/
